aded00b837ca8cae2f71e57b35257ef5a5be17c3
Aditya Srivastava <aditya12497@yahoo.com>
2019-04-12 13:45:03 -0400

    final add of completed files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('starting up!');
+/* eslint-disable new-cap */
+/* eslint-disable camelcase */
+/* eslint-disable template-curly-spacing */
+/* eslint-disable no-multi-spaces */
+import $ from 'jquery';
+// eslint-disable-next-line import/no-unresolved
+import './style.scss';
+
+let counter = 0;
+
+// this function takes care of incrementing the second counter and displaying it on the webpage
+function Sec_Counter() {
+  // eslint-disable-next-line no-plusplus
+  // eslint-disable-next-line template-curly-spacing
+  // eslint-disable-next-line no-plusplus
+  setInterval(() => { $('#main').html(`You have been on this page for ${ counter++  } seconds.`); }, 1000);
+}
+
+Sec_Counter();
